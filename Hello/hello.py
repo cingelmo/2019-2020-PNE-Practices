892880082053f37c503df8b0d7e1dfe63a0ba09c
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-02-04 15:16:45 +0100

    Just another test....

--- Hello/hello.py
+++ Hello/hello.py
@@ -1,2 +1,6 @@
 print('This is my first python program in Pycharm')
-print('Hello world')
+print('Hello world!!!! Changed!!!')
+
+#--Exercise S1-1
+print('Another print added')
+print('just testing...')

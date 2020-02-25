49abbb1eeec0020f5ff09257059e6f94104a929d
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-02-25 12:39:23 +0100

    Seq0

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -1,2 +1,13 @@
+from pathlib import Path
+
+
 def seq_ping():
     return 'OK'
+
+
+def seq_read_fasta(filename):
+    file_contents = Path(filename).read_text()
+    header = file_contents.split('\n')[0]
+    file_clean = file_contents.strip(header).replace('\n', '')[0:20]
+    return file_clean
+

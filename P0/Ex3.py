edb8053131140b814ffe001448dd033d652af4d1
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-03-03 16:40:01 +0100

    Ex3

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -1,5 +1,5 @@
 from P0.Seq0 import *
-filename = ["U5.txt", 'ADA.txt','FRAT1.txt','FXN.txt']
+filename = ["U5.txt", 'ADA.txt', 'FRAT1.txt', 'FXN.txt']
 FOLDER = "../Session 04/"
 for element in filename:
-    print(seq_len(FOLDER + element))
+    print('Gene ', element, '---> Length: ', seq_len(FOLDER + element))

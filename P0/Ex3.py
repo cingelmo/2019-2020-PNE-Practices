15e92a9b92856cb6431d41372ae05ba1664e5f57
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-25 23:45:42 +0200

    Exercise 3

--- P0/Ex3.py
+++ P0/Ex3.py
@@ -1,5 +1,7 @@
 from P0.Seq0 import *
 filename = ["U5.txt", 'ADA.txt', 'FRAT1.txt', 'FXN.txt']
+DOC = '.txt'
 FOLDER = "../Session 04/"
 for element in filename:
+    sequence = seq_read_fasta(FOLDER + element + DOC)
     print('Gene ', element, '---> Length: ', seq_len(FOLDER + element))

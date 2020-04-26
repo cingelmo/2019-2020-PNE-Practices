7c902c2095976c8f7685f12ad73c3de34ffeca3f
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-26 21:45:03 +0200

    Ex5

---
+++ P0/Ex5.py
@@ -0,0 +1,12 @@
+from P0.Seq0 import *
+
+filename = ["U5", 'ADA', 'FRAT1', 'FXN', 'RNU6_269P']
+TypeDOC = '.txt'
+all_bases = ["A", 'C', 'T', 'G']
+FOLDER = "../Session 04/"
+
+print('----| Exercise 5| ----')
+
+for element in filename:
+    sequence = seq_read_fasta(FOLDER + element + TypeDOC)
+    print('Gene', element, ' : ', seq_count(sequence))

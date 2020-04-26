3155d073b0e4310a43a54c2da59b7a942b6df7c0
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-26 21:43:50 +0200

    Ex 2

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,4 +1,6 @@
 from P0.Seq0 import *
+
 filename = "U5.txt"
 FOLDER = "../Session 04/"
+print('----| Exercise 2| ----')
 print('DNA file: U5.txt', '\nThe first 20 bases are:', seq_read_fasta(FOLDER+filename))

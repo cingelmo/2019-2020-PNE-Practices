2aa62927112b39e88dd73f4eb71e30218a22d7e7
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-26 21:45:53 +0200

    Seq0

--- P0/Seq0.py
+++ P0/Seq0.py
@@ -6,8 +6,19 @@ def seq_ping():
 
 
 def seq_read_fasta(filename):
-    file_contents = Path(filename).read_text()
-    header = file_contents.split('\n')[0]
-    file_clean = file_contents.strip(header).replace('\n', '')[0:20]
-    return file_clean
+    file_contents = Path(filename).read_text().split('\n')[1:]
+    sequence = "".join(file_contents)
+    return sequence
 
+
+def seq_len(sequence):
+    return len(sequence)
+
+
+def seq_count_base(sequence, base):
+    return sequence.count(base)
+
+
+def seq_count(sequence):
+    d = {'A': seq_count_base(sequence, 'A'), 'C': seq_count_base(sequence, 'C'), 'T': seq_count_base(sequence, 'T'), 'G': seq_count_base(sequence, 'G')}
+    return d

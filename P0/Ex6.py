196baaa3ba870ddfd1b312a802ad56a919f364a1
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-27 02:26:55 +0200

    Ex6

---
+++ P0/Ex6.py
@@ -0,0 +1,11 @@
+from P0.Seq0 import*
+
+FOLDER = "../Session 04/"
+File = ['U5']
+TypeDOC = '.txt'
+
+print('----| Exercise 6| ----')
+
+for element in File:
+    sequence = seq_read_fasta(FOLDER + element + TypeDOC)[0:20]
+    print('Gene', element, ' : ', '\nFrag: ', sequence, '\nRev : ', seq_reverse(sequence))

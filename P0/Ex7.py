308d5b1b1752d3422071c28faa93c157c62fdeeb
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-27 02:56:03 +0200

    Ex7

---
+++ P0/Ex7.py
@@ -0,0 +1,11 @@
+from P0.Seq0 import*
+
+FOLDER = "../Session 04/"
+File = ['U5']
+TypeDOC = '.txt'
+
+print('----| Exercise 7| ----')
+
+for element in File:
+    sequence = seq_read_fasta(FOLDER + element + TypeDOC)[0:20]
+    print('Gene', element, ' : ', '\nFrag: ', sequence, '\nComp: ', seq_complement(sequence))

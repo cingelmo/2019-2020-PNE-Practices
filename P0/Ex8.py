18d72ba9bb4b74b4452a1c34809b7aa453f46f36
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-27 13:17:12 +0200

    Ex8

---
+++ P0/Ex8.py
@@ -0,0 +1,15 @@
+from P0.Seq0 import*
+
+FOLDER = "../Session 04/"
+Filename = ["ADA", "FRAT1", "FXN", "RNU6_269P", "U5"]
+TypeDOC = '.txt'
+bases = ['A', 'C', 'G', 'T']
+
+print('----| Exercise 8| ----')
+i = 0
+for element in Filename:
+    sequence = seq_read_fasta(FOLDER + element + TypeDOC)
+    Dictionary_bases = seq_count(sequence)
+    Amount_bases = list(Dictionary_bases.values())
+    Most_freq = max(Amount_bases)
+    print('Gene: ', element, 'Most frequent base: ', bases[Amount_bases.index(Most_freq)])

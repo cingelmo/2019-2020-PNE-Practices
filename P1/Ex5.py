0cbea6d16f2c8b54cd098f430940f87feddd3058
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-28 14:17:30 +0200

    Practice 1 - Ex5

--- P1/Ex5.py
+++ P1/Ex5.py
@@ -3,7 +3,7 @@ from P1.Seq1 import Seq
 print("-----| Practice 1, Exercise 5 |------")
 seq0 = Seq()
 seq1 = Seq("ACTGA")
-seq2 = Seq("AFCTGS")
+seq2 = Seq("Invalid sequence")
 
 bases = ["A", "C", "G", "T"]
 

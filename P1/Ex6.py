9f8e8916da3ab4ad881fc66eaae7368c4c3bb717
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-28 12:54:09 +0200

    Practice 1 - Ex6

---
+++ P1/Ex6.py
@@ -0,0 +1,12 @@
+from P1.Seq1 import Seq
+
+print("-----| Practice 1, Exercise 5 |------")
+seq0 = Seq()
+seq1 = Seq("ACTGA")
+seq2 = Seq("AFCTGS")
+
+bases = ["A", "T", "C", "G"]
+
+for seq, value in enumerate([seq0, seq1, seq2]):
+    print("Sequence ", seq, ':', "(Length: ", value.len(), ')', value)
+    print("Bases:", value.seq_count())

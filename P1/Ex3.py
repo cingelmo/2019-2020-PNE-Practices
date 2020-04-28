c73511aee3e2a0fb992e70520d1cdde8145be136
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-28 12:19:48 +0200

    Practice 1 - Ex3

---
+++ P1/Ex3.py
@@ -0,0 +1,10 @@
+from P1.Seq1 import Seq
+
+print('-----| Practice 1, Exercise 3 |------')
+seq1 = Seq()
+seq2 = Seq("ACTGA")
+seq3 = Seq("Invalid sequence")
+
+print("Sequence 1:", seq1)
+print("Sequence 2:", seq2)
+print("Sequence 3:", seq3)

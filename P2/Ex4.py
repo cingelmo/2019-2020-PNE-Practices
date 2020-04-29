d7b8bb6e71cbf25e648218e9e14cb6273789a062
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-30 00:12:26 +0200

    Practice 2 - Ex4

---
+++ P2/Ex4.py
@@ -0,0 +1,15 @@
+from P2.Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 1
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+
+IP = "127.0.0.1"
+PORT = 8080
+
+c = Client(IP, PORT)
+print(c)
+c.debug_talk("Message 1---")
+c.debug_talk("Message 2: Testing !!!")

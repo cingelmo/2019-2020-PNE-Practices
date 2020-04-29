421107ebe04ab269a4fdd0072ca81344c4b52889
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-30 00:11:54 +0200

    Practice 2 - Ex2

---
+++ P2/Ex2.py
@@ -0,0 +1,14 @@
+from P2.Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 1
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+
+IP = "127.0.0.1"
+PORT = 8080
+
+# -- Create a client object
+c = Client(IP, PORT)
+print(c)

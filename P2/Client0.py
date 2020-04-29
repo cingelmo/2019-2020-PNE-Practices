2e8b829f97282a51a06fb7b7aa8e85a57d973f6a
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-04-30 00:11:33 +0200

    ClientO

--- P2/Client0.py
+++ P2/Client0.py
@@ -3,16 +3,24 @@ import termcolor
 
 
 class Client:
+    def ping(self):
+        print("OK!")
+
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
 
-
-    def ping():
-        print("OK!")
-
     def __str__(self):
         return f"Connection to SERVER at {self.ip}, PORT: {self.port}"
+
+    def talk(self, msg):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((self.ip, self.port))
+        s.send(str.encode(msg))
+        response = s.recv(2048).decode("utf-8")
+        s.close()
+
+        return response
 
     def debug_talk(self, msg_to_server):
         msg_from_server = self.talk(msg_to_server)
@@ -22,14 +30,3 @@ class Client:
         termcolor.cprint(msg_from_server, "green")
 
         return msg_from_server
-
-    def talk(self, msg):
-        """Sent the string (message) to the app running in the given IP and Port"""
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((self.ip, self.port))
-        s.send(str.encode(msg))
-        response = s.recv(2048).decode("utf-8")
-        s.close()
-
-        # Return the response
-        return response

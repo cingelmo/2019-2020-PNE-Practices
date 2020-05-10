9f22f9fcdaabf4eb443005d140260c6d6b85b6e6
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-05-10 11:05:07 +0200

    Session 14 - Server 3

--- Session 14/webserver3.py
+++ Session 14/webserver3.py
@@ -10,7 +10,7 @@ socketserver.TCPServer.allow_reuse_address = True
 
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inheritates all his methods and properties
+# It means that our class inherits all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -52,5 +52,5 @@ with socketserver.TCPServer(("", PORT), Handler) as httpd:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("")
-        print("Stoped by the user")
+        print("Stopped by the user")
         httpd.server_close()

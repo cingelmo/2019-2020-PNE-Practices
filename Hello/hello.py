cf7c54c3f3e1b2b1ca13adcc090f54171d99b2e2
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-05-10 19:34:09 +0200

    Hello Trial

--- Hello/hello.py
+++ Hello/hello.py
@@ -1,6 +1,6 @@
 print('This is my first python program in Pycharm')
 print('Hello world!!!! Changed!!!')
 print('I am adding another comment to the hello.py program')
-#--Exercise S1-1
+# --Exercise S1-1
 print('Another print added')
 print('just testing...')

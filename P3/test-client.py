091bda7a217b792fb53174840fc3aca6e9f394a8
cingelmo <c.ingelmo.2019@alumnos.urjc.es>
2020-05-11 12:30:20 +0200

    Practice 3 - Client

--- P3/test-client.py
+++ P3/test-client.py
@@ -1,51 +1,30 @@
 from Client0 import Client
 
-PRACTICE = 3
-EXERCISE = 7
+print(f"-----| Practice 3, Exercise 7 |------")
 
-print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
-
-# -- Parameters of the server to talk to
 IP = "127.0.0.1"
 PORT = 8080
 
-# -- Cofigure the client
 c = Client(IP, PORT)
 print(c)
 
-# -- Test 1: Ping
 print("* Testing PING...")
 print(c.talk("PING"))
 
-# -- Test 2: Get
 print("* Testing GET...")
 for i in range(5):
-    cmd = f"GET {i}"
-    print(f"{cmd}: {c.talk(cmd)}", end="")
+    print(c.talk(f'GET {i}'))
 
-# -- Test 3: INFO
-# -- Get the sequence 0 for testing
-seq = c.talk("GET 0")
-print()
 print("* Testing INFO...")
-cmd = f"INFO {seq}"
-print(c.talk(cmd))
+print(c.talk(f"INFO {seq}"))
 
-# -- Test 4: COMP
 print("* Testing COMP...")
-cmd = f"COMP {seq}"
-print(cmd, end="")
-print(c.talk(cmd))
+print(c.talk(f"COMP {seq}"))
 
-# -- Test 5: REV
 print("* Testing REV...")
-cmd = f"REV {seq}"
-print(cmd, end="")
-print(c.talk(cmd))
+print(c.talk(f"REV {seq}"))
 
-# -- Test 6: GENE
 print("* Testing GENE...")
 for gene in ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]:
-    cmd = f"GENE {gene}"
-    print(cmd)
-    print(c.talk(cmd))
+    print(f"GENE {gene}")
+    print(c.talk(f"GENE {gene}"))
